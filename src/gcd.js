df7bc901f91a666ba26050c113385f49d0945e4f
wcgregory <129503132+wcgregory@users.noreply.github.com>
2024-06-10 15:12:32 +0000

    gcd recursive problem with defined var, resolved after google review

--- src/gcd.js
+++ src/gcd.js
@@ -2,11 +2,15 @@
  * Works out the greatest common divisor of two numbers
  * Uses the euclidean algorithm: https://www.youtube.com/watch?v=JUzYl1TYMcU
  * e.g. gcd(8, 12) => 4
+ * I had to google the answer to this one ;) as it was bugging me!!
+ * https://www.freecodecamp.org/
  */
 function gcd(a, b) {
-  while (a !== 0) {
-    let temp = a
-    a = b % a
+  var r
+  while ((a % b) > 0) {
+    r = a % b
+    a = b
+    b = r
   }
   return b
 }
